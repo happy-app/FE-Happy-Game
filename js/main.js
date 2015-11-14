7f66c4b0a90db45041197fb1ab3a51757b4c0758
Jontae Pierce <jontae.pierce@gmail.com>
2015-11-14 17:06:23 -0500

    OMG IT WORKS!!!!!!1111!!!

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ import 'angular-ui-router';
 import 'angular-material';
 import 'angular-animate';
 import 'angular-aria';
+import $ from 'jquery';
+
+
 
 //  Config File
 import config from './config';
@@ -10,13 +13,58 @@ import config from './config';
 //  Controller Files
 import GameController from './controllers/gamecontroller';
 
+var floatingWindow = {
+	dragstart_handler: function(event) {
+    var originalEvent = event.originalEvent,
+        dataTransfer = event.originalEvent.dataTransfer;
+        dataTransfer.setData("id", this.id);
+        dataTransfer.setData("offsetX", this.offsetLeft - originalEvent.pageX);
+        dataTransfer.setData("offsetY", this.offsetTop - originalEvent.pageY);
+    },
+    dragover_handler: function(event) {
+      event.preventDefault();
+    },
+    drop_handler: function(event) {
+      event.preventDefault();
+      var originalEvent = event.originalEvent,
+          dataTransfer = event.originalEvent.dataTransfer,
+          $container = $(this),
+          topMax = $container.height(),
+          leftMax = $container.width(),
+          topRequested = originalEvent.pageY + parseInt(dataTransfer.getData("offsetY")),
+          leftRequested = originalEvent.pageX + parseInt(dataTransfer.getData("offsetX")),
+          $element = $("#" + dataTransfer.getData("id")),
+          elementHeight = $element.outerHeight(),
+          elementWidth = $element.outerWidth()
+      ;
+
+      // Adapt the requested position so the element fit the container.
+      if (topRequested < 0) topRequested = 0;
+      if (elementHeight + topRequested > topMax) topRequested = topMax - elementHeight;
+      if (leftRequested < 0) leftRequested = 0;
+      if (elementWidth + leftRequested > leftMax) leftRequested = leftMax - elementWidth;
+
+      $element
+        .css({
+          top: topRequested,
+          left: leftRequested
+        });
+    }
+};
+
+$(document).ready(function(){
+  $(".ppiece1").on("dragstart", floatingWindow.dragstart_handler);
+  $(".ppiece2").on("dragstart", floatingWindow.dragstart_handler);
+  $(".ppiece3").on("dragstart", floatingWindow.dragstart_handler);
+  $(".ppiece4").on("dragstart", floatingWindow.dragstart_handler);
+	$(".container").on("dragover", floatingWindow.dragover_handler);
+	$(".container").on("drop", floatingWindow.drop_handler);
+});
 
 
 
-
-
-angular
-.module('app', ['ui.router', 'ngMaterial'])
-.config(config)
-.controller('GameController', GameController)
-;
+// angular
+// .module('app', ['ui.router', 'ngMaterial'])
+// .config(config)
+// .controller('GameController', GameController)
+// ;

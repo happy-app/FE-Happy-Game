ab9d3a488763f07be918647e6fdd6aa25ce6ddeb
Jontae Pierce <jontae.pierce@gmail.com>
2015-11-14 18:55:03 -0500

    YEAH BOI

--- app/js/main.js
+++ app/js/main.js
@@ -35,17 +35,13 @@ var GameController = function GameController($scope) {
 
   var i;
   $scope.itemsList = {
-    items1: [],
-    items2: []
+    items1: []
   };
 
-  for (i = 0; i <= 5; i += 1) {
+  for (i = 0; i <= 8; i += 1) {
     $scope.itemsList.items1.push({ 'Id': i, 'Label': 'Item ' + i });
   }
 
-  for (i = 0; i <= 5; i += 1) {
-    $scope.itemsList.items2.push({ 'Id': i, 'Label': 'Item 2_' + i });
-  }
   $scope.sortableOptions = {
     containment: '#sortable-container',
     //restrict move across columns. move only within column.
